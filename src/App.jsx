92aebd8112bcb1c3df94a8ff2cc0b7cb2ea8bfe9
Pete K <petek@fastmail.com>
2019-11-01 02:22:55 -0400

    added search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,20 +1,37 @@
 import React from 'react';
-import { Container, Segment } from 'semantic-ui-react';
+import { Container, Segment, Header } from 'semantic-ui-react';
 
 import Searchbar from './components/search/Searchbar';
 
 import './assets/css/App.css';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = { searchTerm: '', videos: [], page: 1 };
+  }
+
+  fetchResults = (searchTerm) => {
+console.log('searchTerm', searchTerm);
+  } 
+
   render() {
+    let searchResponse = '';
+
+    if (this.state.searchTerm.length > 0) {
+      searchResponse = (
+        <Header as="h3">You searched for: {this.state.searchTerm}</Header>
+      );
+    }
     return (
       <Container className="mainContent">
         <Segment.Group>
           <Segment>
-            <Searchbar />
+            <Searchbar fetchResults={this.fetchResults} />
           </Segment>
           <Segment>
-            Main body
+            {searchResponse}
           </Segment>
         </Segment.Group>
     </Container>
--- src/components/search/Searchbar.jsx
+++ src/components/search/Searchbar.jsx
@@ -1,10 +1,49 @@
 import React from 'react';
-import { Container } from 'semantic-ui-react';
+import { Input, Grid } from 'semantic-ui-react';
 
-const Searchbar = () => (
-   <Container textAlign={"center"}>
-      Insert Searchbar
-   </Container>
-)
+class Searchbar extends React.Component {
+   constructor(props) {
+      super(props);
+
+      this.state = { searchTerm: '' };
+   }
+
+   handleChange = e => this.setState({ [e.target.name]: e.target.value })
+
+   keyPress = (e) => {
+      if(e.keyCode === 13){
+         this.search();
+     }
+   }
+
+   search = () => {
+      this.props.fetchResults(this.state.searchTerm);
+   }
+
+   render() {
+     return (
+         <Grid>
+            <Grid.Row>
+               <Grid.Column width={2} verticalAlign={'middle'}>
+                  <img src={process.env.PUBLIC_URL + '/images/yt_logo_rgb_light.png'} alt="YouTube logo" className="logo" />
+               </Grid.Column>
+               <Grid.Column textAlign={'center'} width={14}>
+                  <Input
+                     name = "searchTerm"
+                     placeholder = "Search..."
+                     fluid
+                     onKeyDown = {this.keyPress}
+                     onChange = {this.handleChange}
+                     action = {{
+                        icon: 'search',
+                        onClick: () => this.search()
+                     }}
+                  />
+               </Grid.Column>
+            </Grid.Row>
+         </Grid>
+     );
+  }
+}
 
 export default Searchbar;
